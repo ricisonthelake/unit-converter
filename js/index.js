4e482898faa36af9f9d5cdb5df4777631bc00976
ricisonthelake <riconthelake@gmail.com>
2025-02-27 20:01:47 -0500

    early commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+const meterToFeet = 3.281
+const litreToGallon = 0.264
+const lilogramToPound = 2.204
+const convertBtn = document.getElementById("convert-btn")
+let numberToConvertEl = document.getElementById("number-to-convert")
+let 
