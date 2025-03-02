a1c8e7e084853ba1e435b48b7084e5941c04039e
ricisonthelake <riconthelake@gmail.com>
2025-03-02 09:48:04 -0500

    early Commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
 const meterToFeet = 3.281
 const litreToGallon = 0.264
-const lilogramToPound = 2.204
-const convertBtn = document.getElementById("convert-btn")
+const kilogramToPound = 2.204
+const convertBtn = document.getElementById("convert-button")
 let numberToConvertEl = document.getElementById("number-to-convert")
-let 
+let convertedLengthEl = document.getElementById("converted-length")
+let convertedVolumeEl = document.getElementById("converted-volume")
+let convertedMassEl = document.getElementById("converted-mass")
+
+
+convertBtn.addEventListener("click", function () {
+
+    let baseValue = numberToConvertEl.value
+    numberToConvertEl.value = ""
+
+
+    convertedLengthEl.innerHTML = `${baseValue} meter = ${(baseValue * meterToFeet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)} meters`
+
+    convertedVolumeEl.innerHTML = `${baseValue} litres = ${(baseValue * litreToGallon).toFixed(3)} gallons | ${baseValue} gallons = ${(baseValue / litreToGallon).toFixed(3)} litres`
+
+    convertedMassEl.innerHTML = `${baseValue} kilos = ${(baseValue * kilogramToPound).toFixed(3)} pounds | ${baseValue} pounds = ${(baseValue / kilogramToPound).toFixed(3)} kilos`
+
+
+
+
+})
